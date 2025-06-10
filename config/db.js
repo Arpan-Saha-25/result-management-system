1eb20006ea90a7c6af1b11fb9be6fcb84bd26736
ARPAN SAHA <arpansaha004@gmail.com>
2025-06-10 22:58:36 +0530

    improved

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 // config/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/studentResultDB', {
+mongoose.connect('mongodb+srv://pymongo:projectmongo@cluster0.n6ubcpo.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
